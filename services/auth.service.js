7829c5dd8997250bfd03ae12882cbee62094594c
XphyreInc <xphyreinc@gmail.com>
2025-01-02 19:00:33 +0500

    Login Modified

--- services/auth.service.js
+++ services/auth.service.js
@@ -5,16 +5,16 @@ const sql = require("../db.service.js/queries.service");
 module.exports = {
 
     // TO RESGISTER USERS
-    
-    
+
+
     async register(userDetail) {
         try {
             const { first_name, last_name, email, password, role, phone_number } = userDetail;
-    
+
             // Check if the user is already registered
             const result = await pool.query(sql.CHECK_USER_REGISTERED, [email]);
             const isUserRegistered = result.rows;
-    
+
             if (!isUserRegistered.length) {
                 // Insert the new user into the database and return the ID
                 const insertResult = await pool.query(sql.INSERT_INTO_USERS, [
@@ -25,9 +25,9 @@ module.exports = {
                     role,
                     phone_number
                 ]);
-    
+
                 const registeredUserId = insertResult.rows[0].id; // Get the ID of the newly registered user
-    
+
                 return { message: "User Created Successfully", userId: registeredUserId };
             } else {
                 return { message: "User Already Registered" };
@@ -36,145 +36,30 @@ module.exports = {
             console.error("Error creating user:", error);
             throw error;
         }
-    }
-    
-    ,
-    //get user by id
-    // async getUserById(userId) {
-    //     try {
-    //         const [user] = await pool.query(sql.GET_USER_BY_ID, [userId]);
-
-    //         if (user.length === 1) {
-    //             const { role } = user[0];
-
-    //             switch (role) {
-    //                 case 1: // Admin
-    //                     return async () => {
-    //                         try {
-    //                             const [adminDetails] = await pool.query(sql.GET_ADMIN_DETAILS, [
-    //                                 userId,
-    //                             ]);
-    //                             return { user: adminDetails[0], roleDetails: adminDetails[0] };
-    //                         } catch (error) {
-    //                             throw error;
-    //                         }
-    //                     };
-    //                 case 2: // Instructor
-    //                     return async () => {
-    //                         try {
-    //                             const [instructorDetails] = await pool.query(
-    //                                 sql.GET_INSTRUCTOR_DETAILS,
-    //                                 [userId]
-    //                             );
-    //                             return {
-    //                                 user: instructorDetails[0],
-    //                                 roleDetails: instructorDetails[0],
-    //                             };
-    //                         } catch (error) {
-    //                             throw error;
-    //                         }
-    //                     };
-    //                 case 3: // Student
-    //                     return async () => {
-    //                         try {
-    //                             const [studentDetails] = await pool.query(
-    //                                 sql.GET_STUDENT_DETAILS,
-    //                                 [userId]
-    //                             );
-    //                             return {
-    //                                 user: studentDetails[0],
-    //                                 roleDetails: studentDetails[0],
-    //                             };
-    //                         } catch (error) {
-    //                             throw error;
-    //                         }
-    //                     };
-    //                 default:
-    //                     return null; // Unknown role
-    //             }
-    //         } else {
-    //             return null; // User not found
-    //         }
-    //     } catch (error) {
-    //         throw error;
-    //     }
-    // },
-
-    //Edit user by id
-    // async editUserById(userId, updatedUserData) {
-    //     try {
-    //         const { role, admin_type } = updatedUserData;
-
-    //         switch (role) {
-    //             case 1: // Admin
-    //                 async () => {
-    //                     try {
-    //                         await pool.query(sql.UPDATE_ADMIN_TYPE, [admin_type, userId]);
-    //                         await pool.query(sql.EDIT_ADMIN_DETAILS, [
-    //                             updatedUserData,
-    //                             userId,
-    //                         ]);
-    //                     } catch (error) {
-    //                         throw error;
-    //                     }
-    //                 };
-    //                 break;
-    //             case 2: // Instructor
-    //                 async () => {
-    //                     try {
-    //                         await pool.query(sql.EDIT_INSTRUCTOR_DETAILS, [
-    //                             updatedUserData,
-    //                             userId,
-    //                         ]);
-    //                     } catch (error) {
-    //                         throw error;
-    //                     }
-    //                 };
-    //                 break;
-    //             case 3: // Student
-    //                 async () => {
-    //                     try {
-    //                         await pool.query(sql.EDIT_STUDENT_DETAILS, [
-    //                             updatedUserData,
-    //                             userId,
-    //                         ]);
-    //                     } catch (error) {
-    //                         throw error;
-    //                     }
-    //                 };
-    //                 break;
-    //             default:
-    //                 return "Unknown role";
-    //         }
-
-    //         return "User details updated successfully";
-    //     } catch (error) {
-    //         throw error;
-    //     }
-    // },
+    },
 
     // SIGN IN
     async signIn(email, password, role) {
         try {
-        
-        
+
+
             if (role === 'MABK') {
-                
+
                 const result = await pool.query(sql.LOGIN_USER, [
                     email,
                     password,
                     role
                 ]);
-                
+
                 // Check if there are any results in the rows array
                 if (result.rows.length === 0) {
                     throw new Error("Invalid email, password, or role.");
                 }
-    
+
                 // Return the manager user data
                 const userData = result.rows[0];
                 console.log("Sign-in successful for Manager:", userData.first_name, userData.last_name);
-            
+
                 return {
                     user_id: userData.id || "",
                     first_name: userData.first_name || "",
@@ -199,15 +84,15 @@ module.exports = {
                     password,
                     role
                 ]);
-                
+
                 // Check if there are any results in the rows array
                 if (result.rows.length === 0) {
                     throw new Error("Invalid email, password, or role.");
                 }
-                
+
                 // Return the user data
                 const userData = result.rows[0];
-                
+
                 return {
                     user_id: userData.id || "",
                     first_name: userData.first_name || "",
@@ -215,20 +100,29 @@ module.exports = {
                     email: userData.email || "",
                     password: userData.password || "",
                     role: userData.role || "",
-                    phone_number: userData.phone_number || ""
+                    phone_number: userData.phone_number || "",
+                    callcenter_id: userData.callcenter_id || "",
+                    center_name: userData.center_name || "",
+                    country: userData.country || "",
+                    owner_phone_no: userData.owner_phone_no || "",
+                    authorized_person: userData.authorized_person || "",
+                    center_email: userData.center_email || "",
+                    phone_number: userData.phone_number || "",
+                    phone_number: userData.phone_number || "",
+                    phone_number: userData.phone_number || "",
                 };
                 console.log("sign in successfull");
-                
+
             } else {
                 // Invalid role
                 throw new Error("Invalid role.");
-            }  
+            }
         } catch (error) {
             console.error("Error signing in:", error.message);
             throw error;
         }
     }
-    
-    
-    
+
+
+
 };
