4119886383b0759911388c9d15ab7f1460505bd6
XphyreInc <xphyreinc@gmail.com>
2025-01-17 21:47:24 +0500

    db error

--- db.service.js/db.conn.js
+++ db.service.js/db.conn.js
@@ -11,6 +11,8 @@ async function createDatabase() {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
+            port: 5432,
+            ssl: { rejectUnauthorized: false },
         });
 
         // Connect to the PostgreSQL server
